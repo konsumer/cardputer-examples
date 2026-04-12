992e06d2fd4b53fc645a9dd03cf3febb9a08b7e3
David Konsumer <konsumer@jetboystudio.com>
2026-04-11 18:25:31 -0700

    better keyboard demo

--- src/keyboard/README.md
+++ src/keyboard/README.md
@@ -1,6 +1,6 @@
 # keyboard
 
-This tests basic keyboard action.
+This tests basic keyboard action. It's sort of a fake chat screen.
 
 ```sh
 pio run --target upload -e keyboard
--- src/keyboard/main.cpp
+++ src/keyboard/main.cpp
@@ -1,53 +1,66 @@
 #include "M5Cardputer.h"
 
 M5Canvas canvas(&M5Cardputer.Display);
-String data = "> ";
+std::string data = "";
+
+// clear text area & draw current input
+void drawPrompt() {
+  M5Cardputer.Display.fillRect(0, M5Cardputer.Display.height() - 12, M5Cardputer.Display.width(), 12, BLACK);
+  M5Cardputer.Display.setCursor(4, M5Cardputer.Display.height() - 10);
+  M5Cardputer.Display.printf("> %s", data.c_str());
+}
+
+// add scrolled text attributed to a person, with a color
+void say(std::string name, std::string message, int color = YELLOW) {
+  canvas.setTextColor(color);
+  canvas.printf("%s:", name.c_str());
+  canvas.setTextColor(WHITE);
+  canvas.println(message.c_str());
+  canvas.pushSprite(8, 20);
+}
 
 void setup() {
   auto cfg = M5.config();
   M5Cardputer.begin(cfg, true);  // enableKeyboard
   M5Cardputer.Display.setRotation(1);
-  M5Cardputer.Display.setTextSize(0.5);
-  M5Cardputer.Display.setTextFont(&fonts::FreeSerifBoldItalic18pt7b);
 
-  M5Cardputer.Display.drawRect(0, 0, M5Cardputer.Display.width(), M5Cardputer.Display.height() - 28, GREEN);
-  M5Cardputer.Display.fillRect(0, M5Cardputer.Display.height() - 4, M5Cardputer.Display.width(), 4, GREEN);
+  // prompt and input text are this color
+  M5Cardputer.Display.setTextColor(WHITE);
 
-  canvas.setTextSize(0.5);
-  canvas.setTextFont(&fonts::FreeSerifBoldItalic18pt7b);
+  // draw outer UI directly on screen
+  M5Cardputer.Display.drawCenterString("Chat Demo", M5Cardputer.Display.width()/2, 4);
+  M5Cardputer.Display.drawRect(4, 18, M5Cardputer.Display.width() - 8, M5Cardputer.Display.height() - 34, BLUE);
+
+  // scrolling text goes into a canvas
+  canvas.createSprite(M5Cardputer.Display.width() - 16, M5Cardputer.Display.height() - 38);
   canvas.setTextScroll(true);
 
-  canvas.createSprite(M5Cardputer.Display.width() - 8, M5Cardputer.Display.height() - 36);
-  canvas.println("Press Key and Enter to Input Text");
-  canvas.pushSprite(4, 4);
+  // add an initial messages from "system"
+  say("System", "Type your messages.", ORANGE);
+  say("System", "This will scroll.", ORANGE);
 
-  M5Cardputer.Display.drawString(data, 4, M5Cardputer.Display.height() - 24);
+  // draw initial
+  drawPrompt();
 }
 
 void loop() {
   M5Cardputer.update();
 
-  if (M5Cardputer.Keyboard.isChange()) {
-    if (M5Cardputer.Keyboard.isPressed()) {
-      Keyboard_Class::KeysState status = M5Cardputer.Keyboard.keysState();
-
-      for (auto i : status.word) {
-        data += i;
-      }
-
-      if (status.del) {
-        data.remove(data.length() - 1);
-      }
-
-      if (status.enter) {
-        data.remove(0, 2);
-        canvas.println(data);
-        canvas.pushSprite(4, 4);
-        data = "> ";
-      }
-
-      M5Cardputer.Display.fillRect(0, M5Cardputer.Display.height() - 28, M5Cardputer.Display.width(), 25, BLACK);
-      M5Cardputer.Display.drawString(data, 4, M5Cardputer.Display.height() - 24);
+  // handle input
+  if (M5Cardputer.Keyboard.isChange() && M5Cardputer.Keyboard.isPressed()) {
+    Keyboard_Class::KeysState status = M5Cardputer.Keyboard.keysState();
+    for (auto i : status.word) {
+      data += i;
     }
+    if (status.del) {
+      if (!data.empty()) {
+        data.pop_back();
+      }
+    }
+    if (status.enter) {
+      say("You", data);
+      data = "";
+    }
+    drawPrompt();
   }
 } 
