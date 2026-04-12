6b67a6ab5d11e5d68f71af4d5b06a49372fc75f6
David Konsumer <konsumer@jetboystudio.com>
2026-04-11 19:36:04 -0700

    simplify display

--- src/display/main.cpp
+++ src/display/main.cpp
@@ -1,7 +1,5 @@
 #include "M5Cardputer.h"
 
-LovyanGFX* gfx = &M5Cardputer.Display;
-
 void setup() {
   auto cfg = M5.config();
   M5Cardputer.begin(cfg);
@@ -20,6 +18,6 @@ void loop() {
   uint16_t c = rand();
   
   M5Cardputer.Display.fillCircle(x, y, r, c);
-  // gfx->fillRect(x - r, y - r, r * 2, r * 2, c);
+  // M5Cardputer.Display.fillRect(x - r, y - r, r * 2, r * 2, c);
 }
 
