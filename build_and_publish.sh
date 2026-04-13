1bfd6ddcd5e3794b2178317b22dac9126fcb8fea
David Konsumer <konsumer@jetboystudio.com>
2026-04-13 06:42:13 -0700

    conditional build

--- build_and_publish.sh
+++ build_and_publish.sh
@@ -2,6 +2,9 @@
 set -e
 
 # Loop through all projects & build/publish (on m5burner)
+# run with "publish" arg to publish
+
+mode="${1}"
 
 mkdir -p dist
 
@@ -14,6 +17,8 @@ for i in src/*;do
 	cp .pio/build/${name}/firmware.bin dist/${name}.bin
 	cp src/${name}/${name}.png dist/
 	cp src/${name}/m5burner.json dist/${name}-m5burner.json
-	echo "Publishing ${title}"
-	npx -y m5-burner@latest publish-firmware dist/${name}-m5burner.json
+	if [ "${mode}"  == "publish" ];then
+		echo "Publishing ${title}"
+		npx -y m5-burner@latest publish-firmware dist/${name}-m5burner.json
+	fi
 done
