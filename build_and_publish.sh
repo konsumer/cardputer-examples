8467233e26eae548e80c71e85fb96a581be76e30
David Konsumer <konsumer@jetboystudio.com>
2026-04-13 06:34:50 -0700

    more output in build

--- build_and_publish.sh
+++ build_and_publish.sh
@@ -7,9 +7,13 @@ mkdir -p dist
 
 for i in src/*;do
 	name=$(basename $i)
+	title="$(jq -r .fields.name src/${name}/m5burner.json)"
+	echo "Building ${title}"
 	pio run -e "${name}"
+	echo "Copying ${title}"
 	cp .pio/build/${name}/firmware.bin dist/${name}.bin
 	cp src/${name}/${name}.png dist/
 	cp src/${name}/m5burner.json dist/${name}-m5burner.json
+	echo "Publishing ${title}"
 	npx -y m5-burner@latest publish-firmware dist/${name}-m5burner.json
 done
